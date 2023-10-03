7b356776c15e61d8c2287da33953e0a2492c194d
edriso <edriso.dev@gmail.com>
2023-10-03 17:40:46 +0300

    add '.' at the end of the msg

--- server/utils/sendVerificationEmail.js
+++ server/utils/sendVerificationEmail.js
@@ -3,7 +3,7 @@ const sendEmail = require('./sendEmail');
 const sendVerificationEmail = async ({ name, email, verificationToken }) => {
   const verificationLink = `${process.env.FRONTEND_BASE_URL}/user/verify-email?token=${verificationToken}&email=${email}`;
 
-  const message = `<p>Please confirm your email by clicking on the following link: <a href="${verificationLink}">Verify Email</a></p>`;
+  const message = `<p>Please confirm your email by clicking on the following link: <a href="${verificationLink}">Verify Email</a>.</p>`;
 
   return sendEmail({
     to: email,
