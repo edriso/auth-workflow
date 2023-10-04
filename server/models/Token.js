09b0f573830277033226dbe02982d458eef678d3
edriso <edriso.dev@gmail.com>
2023-10-04 21:01:16 +0300

    add a comment about isValid property

--- server/models/Token.js
+++ server/models/Token.js
@@ -15,6 +15,7 @@ const tokenSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    // We can prevent user from log in if we set isValid to be false, as we check about that in the log in
     isValid: {
       type: Boolean,
       default: true,
