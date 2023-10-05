2da1e8084056789d7e7964e157925e8c5ca69838
edriso <edriso.dev@gmail.com>
2023-10-05 14:18:16 +0300

    rename verificationURL variable

--- server/utils/sendVerificationEmail.js
+++ server/utils/sendVerificationEmail.js
@@ -1,9 +1,9 @@
 const sendEmail = require('./sendEmail');
 
 const sendVerificationEmail = async ({ name, email, verificationToken }) => {
-  const verificationLink = `${process.env.FRONTEND_BASE_URL}/user/verify-email?token=${verificationToken}&email=${email}`;
+  const verificationURL = `${process.env.FRONTEND_BASE_URL}/user/verify-email?token=${verificationToken}&email=${email}`;
 
-  const message = `<p>Please confirm your email by clicking on the following link: <a href="${verificationLink}">Verify Email</a>.</p>`;
+  const message = `<p>Please confirm your email by clicking on the following link: <a href="${verificationURL}">Verify Email</a>.</p>`;
 
   return sendEmail({
     to: email,
